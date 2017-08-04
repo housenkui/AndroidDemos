25629602a6fb52d27688d1a0ac3758f5288fa86d
侯森魁 <housenkui627@1caifu.com>
2017-08-04 12:23:23 +0800

    Second Demo

---
+++ MyTest02/app/src/main/java/com/example/shidifu/mytest02/MainActivity.java
@@ -0,0 +1,16 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;gitpackage com.example.shidifu.mytest02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shidifu.mytest02.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
